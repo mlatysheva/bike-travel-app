960eaa0871aff3d76c88c1f44459cae554756394
mlatysheva <mlatysheva@gmail.com>
2023-06-20 14:19:48 +0200

    feat: add locations module and update the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from "./main/pages/main-page/main-page.component";
+import {LocationsPageComponent} from "./locations/pages/locations-page/locations-page.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
@@ -7,6 +9,10 @@ const routes: Routes = [
     path: 'main',
     loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
   },
+  {
+    path: 'locations',
+    loadChildren: () => import('./locations/locations.module').then(m => m.LocationsModule),
+  }
 ]
 
 @NgModule({
