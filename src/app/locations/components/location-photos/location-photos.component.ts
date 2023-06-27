a2b29191b09f34999f81beedafceb35498396db6
mlatysheva <mlatysheva@gmail.com>
2023-06-27 15:25:29 +0200

    style: add photos to bikes and style pages

--- src/app/locations/components/location-photos/location-photos.component.ts
+++ src/app/locations/components/location-photos/location-photos.component.ts
@@ -22,27 +22,9 @@ export class LocationPhotosComponent implements OnInit {
     centerMode: true,
   };
 
-  slickInit(e: { event: any; slick: any }) {
-    console.log('slick initialized');
-  }
-
-  breakpoint(e: { event: any; slick: any; breakpoint: any }) {
-    console.log('breakpoint');
-  }
-
-  afterChange(e: { event: any; slick: any; currentSlide: number; first: boolean; last: boolean }) {
-    console.log('afterChange');
-  }
-
-  beforeChange(e: { event: any; slick: any; currentSlide: number; nextSlide: number }) {
-    console.log('beforeChange');
-  }
-
   ngOnInit() {
     this.locationPhotos$.subscribe((photos) => {
         photos.length > 4 ? 4 : photos.length;
-        console.log(photos);
-        console.log(photos.length);
         this.sliderConfig = {
           ...this.sliderConfig,
           slidesToScroll: photos.length > 4 ? 4 : photos.length,
